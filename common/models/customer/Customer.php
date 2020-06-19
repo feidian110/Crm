3e0b826817320a71bf2d1c338756a6670a25394c
feidian110 <97791736@qq.com>
2020-06-19 12:55:39 +0800

    优化：部分功能

--- common/models/customer/Customer.php
+++ common/models/customer/Customer.php
@@ -105,7 +105,7 @@ class Customer extends \common\models\base\BaseModel
             if( !$contact->save() ){
                 throw new \Exception('客户信息有误，存储失败！');
             }
-            Yii::$app->crmService->base->updateActionLog($this->store_id,Yii::$app->user->id,CrmTypeEnum::CUSTOMER,$this->id,'','','创建了客户');
+            //Yii::$app->crmService->base->updateActionLog($this->store_id,Yii::$app->user->id,CrmTypeEnum::CUSTOMER,$this->id,'','','创建了客户');
             $notice = WorkNotice::findOne(['merchant_id'=>$this->merchant_id,'store_id'=>$this->store_id]);
             if ( $notice && $notice['open_notice']== 1 && !empty($notice['customer_key']) ){
                 $arr = [
